de78c8d32747d8fa69e981cacc953fbd7570cfab
Krishna <krishna.venu@georgebrown.ca>
2025-10-04 22:19:14 -0400

    Initial React SPA setup for Lab 05

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ color: "red" }}>🔥 Testing React Rendering 🔥</h1>
+      <p>If you see this in red, React is working!</p>
+    </div>
+  );
+}
+
+export default App;
